a71aa032ccd3c98392462e1c5f13ee37fac430b8
xiaofeng <1244762145@qq.com>
2022-03-03 21:11:19 +0800

    initial commit

--- app/src/main/java/zjut/xiaofeng/myapplication1/MainActivity.java
+++ app/src/main/java/zjut/xiaofeng/myapplication1/MainActivity.java
@@ -6,18 +6,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import zjut.xiaofeng.myapplication1.bean.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
     //分别声明布局文件中用到的变量
-    private EditText username;
-    private EditText password;
+    private EditText usernameEdit;
+    private EditText passwordEdit;
     private RadioButton radioButton_userAgreement;
     private ImageButton button_login;
     private Button button_loginByPhone;
@@ -30,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
     public void initView() {
         pref = PreferenceManager.getDefaultSharedPreferences(this);
         //绑定控件，这样变量就代表了被绑定的控件
-        username = findViewById(R.id.login_username);
-        password = findViewById(R.id.login_password);
+        usernameEdit = findViewById(R.id.login_username);
+        passwordEdit = findViewById(R.id.login_password);
         button_login = findViewById(R.id.btn_login);
         radioButton_userAgreement = findViewById(R.id.login_radioButton_serviceAgreement);
         button_loginByPhone = findViewById(R.id.btn_phone);
@@ -45,8 +47,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        String userName = usernameEdit.getText().toString();
+        String password = passwordEdit.getText().toString();
 
-//        监听是否点击注册按钮，点击则跳转到注册界面
+        /**
+         * 监听是否点击注册按钮，点击则跳转到注册界面
+         */
         button_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View w) {
@@ -57,5 +63,38 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(userName)) {
+                    Toast.makeText(MainActivity.this, "请输入用户名", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(MainActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
+                } else if (!isExistUserInfo(userName, password)) {
+                    Toast.makeText(MainActivity.this, "用户名或密码错误，请重试", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "登陆成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    /**
+     * 从SharedPreferences中读取输入的用户名，判断SharedPreferences中是否有此用户名
+     *
+     * @param userName
+     * @return
+     */
+    private boolean isExistUserInfo(String userName, String password) {
+
+        SharedPreferences sp = getSharedPreferences("loginInfo", MODE_PRIVATE);
+        //获取密码
+        String password_stored = sp.getString(userName, "");//传入用户名获取密码
+        //如果密码不为空则确实保存过这个用户名
+        if (password_stored != null && password_stored.equals(password)) {
+            return true;
+        }
+        return false;
     }
 }
